8e44eb198d642bf5233cf5c33d2764013ae44425
VictorZamudio <victor.zamudio@hotmail.com>
2019-05-31 19:53:26 -0500

    Fixing level issue

--- src/main/kotlin/com/victorzamudio/restfulapi/models/Verb.kt
+++ src/main/kotlin/com/victorzamudio/restfulapi/models/Verb.kt
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotEmpty
 import javax.validation.constraints.NotNull
 
 @Entity(name = "verbs")
@@ -13,13 +14,13 @@ data class Verb(
   strategy = GenerationType.IDENTITY)
  val verb_id: Long = 0,
  @get: NotBlank val verb_meaning: String = "",
-// @get: NotNull val verb_level: Int = 0,
+ @get: NotBlank val verb_level: Long = 0,
  @get: NotBlank val verb_path_img: String = "",
  @get: NotBlank val verb_infinitive: String = "",
  @get: NotBlank val verb_simple_past: String = "",
  @get: NotBlank val verb_past_participle: String = "",
  @get: NotBlank val verb_category: String = "",
- @get: NotBlank val verb_verb_types: String = "",
+ @get: NotBlank val verb_types: String = "",
  @get: NotBlank val verb_examples: String = ""
 )
 {}
