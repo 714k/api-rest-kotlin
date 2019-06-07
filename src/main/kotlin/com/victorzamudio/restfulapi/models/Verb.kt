ae78919b9b93aa8a70e63da4d2e5a676623758c5
VictorZamudio <victor.zamudio@hotmail.com>
2019-06-06 21:02:25 -0500

    Fixing verb_level issue and improving database and table

--- src/main/kotlin/com/victorzamudio/restfulapi/models/Verb.kt
+++ src/main/kotlin/com/victorzamudio/restfulapi/models/Verb.kt
@@ -5,7 +5,6 @@ import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.validation.constraints.NotBlank
-import javax.validation.constraints.NotNull
 
 @Entity(name = "verbs")
 data class Verb(
@@ -13,13 +12,13 @@ data class Verb(
   strategy = GenerationType.IDENTITY)
  val verb_id: Long = 0,
  @get: NotBlank val verb_meaning: String = "",
-// @get: NotNull val verb_level: Int = 0,
+ @get: NotBlank val verb_level: Int = 0,
  @get: NotBlank val verb_path_img: String = "",
  @get: NotBlank val verb_infinitive: String = "",
  @get: NotBlank val verb_simple_past: String = "",
  @get: NotBlank val verb_past_participle: String = "",
  @get: NotBlank val verb_category: String = "",
- @get: NotBlank val verb_verb_types: String = "",
+ @get: NotBlank val verb_types: String = "",
  @get: NotBlank val verb_examples: String = ""
 )
 {}
